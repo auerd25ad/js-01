784e3e0f586e48f072c6b7ae7aeb27566e5561bd
auerd25ad <auerd.25ad@acsjszki.hu>
2026-03-18 10:30:49 +0100

    2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-<script>
-    //alert("Hello, world!")
-</script>
+ //alert("Hello, world!")
+
+ function gomb() {
+    document.getElementById("szoveg").innerHTML = "Hello, world!";
+ }
+
+ function osszead() {
+    var szam1 = parseFloat(document.getElementById("szam1").value)
+    var szam2 = parseFloat(document.getElementById("szam2").value)
+    var eredmeny = szam1 + szam2
+    document.getElementById("eredmeny").innerHTML = eredmeny
+ }
